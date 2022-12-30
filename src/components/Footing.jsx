14a3e51d66308cd6e44a761ea967bbb5d6f3d0f3
Jose Quijije <josequijije5@gmail.com>
2022-12-29 19:13:58 -0600

    project semidone2

--- src/components/Footing.jsx
+++ src/components/Footing.jsx
@@ -4,6 +4,7 @@ const Footing = () => {
   return (
     <div className='flex justify-between items-center  w-full h-20 px-4 text-white bg-black'>
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4'>
+            <p className='font-bold'>Portfolio</p>
             <p className='flex flex-col justify-center align-center font-bold inline text-white '>
             &copy; 2023 josequijije5@gmail.com
             </p>
